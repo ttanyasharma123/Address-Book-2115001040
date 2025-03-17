4639942a3e14a229829851cacf31f46f5f72439d
tanaya <ttanyasharma123@gmail.com>
2025-03-17 23:15:41 +0530

    [Tanya]: Add Section-3 Implemented Successfully

---
+++ Section-3/UC1/BusinessLayer/Service/AddressBookService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using BusinessLayer.Interface;
+using ModelLayer.DTO;
+using ModelLayer.Model.Entities;
+using RepositoryLayer.Interface;
+using RepositoryLayer.Service;
+
+namespace BusinessLayer.Service
+{
+    public class AddressBookService : IAddressBookService
+    {
+        private readonly IAddRL _repository;
+        private readonly IMapper _mapper;
+
+        public AddressBookService(IAddRL repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<AddressBookEntryDTO>> GetAllContactsAsync()
+        {
+            var contacts = await _repository.GetAllContactsAsync();
+            return _mapper.Map<IEnumerable<AddressBookEntryDTO>>(contacts);
+        }
+
+        public async Task<AddressBookEntryDTO> GetContactByIdAsync(int id)
+        {
+            var contact = await _repository.GetContactByIdAsync(id);
+            return contact != null ? _mapper.Map<AddressBookEntryDTO>(contact) : null;
+        }
+
+        public async Task<bool> AddContactAsync(int userId, AddressBookEntryDTO contactDto)
+        {
+            var contact = _mapper.Map<Contact>(contactDto);
+            contact.UserId = userId; // Assign UserId from JWT token
+
+            return await _repository.AddContactAsync(contact);
+        }
+
+        public async Task<bool> UpdateContactAsync(int id, AddressBookEntryDTO contactDto)
+        {
+            var existingContact = await _repository.GetContactByIdAsync(id);
+            if (existingContact == null) return false;
+
+            _mapper.Map(contactDto, existingContact);
+            return await _repository.UpdateContactAsync(existingContact);
+        }
+
+        public async Task<bool> DeleteContactAsync(int id)
+        {
+            return await _repository.DeleteContactAsync(id);
+        }
+
+    }
+}
---
+++ Section-3/UC1/ModelLayer/Model/Entities/Contact.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.Model.Entities
+{
+    [Table("Address")]
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)] // Limiting name length
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(255)] // Email length limit
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\+?[0-9]{10,15}$", ErrorMessage = "Invalid Phone Number")]
+        [MaxLength(15)] // Limiting to avoid overflow
+        public string Phone { get; set; }
+
+        [Required]
+        [MaxLength(500)] // Limiting address length
+        public string Address { get; set; }
+
+        // ✅ Foreign Key for User
+        [Required]
+        public int UserId { get; set; }
+
+        // ✅ Navigation Property (Optional)
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+    }
+}
